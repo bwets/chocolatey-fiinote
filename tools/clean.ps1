c04994da4319d2baed808010db3a8120e8cd4a1c
Gregory Kieffer <gk@bwets.net>
2017-11-22 22:11:18 +0100

    removed comments

---
+++ tools/clean.ps1
@@ -0,0 +1,5 @@
+$f='chocolateyinstall.ps1'
+gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+$f='chocolateyuninstall.ps1'
+gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
