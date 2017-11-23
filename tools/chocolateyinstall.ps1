97c838ef243ddf315d76efa03782661396d7a1df
Gregory Kieffer <gk@bwets.net>
2017-11-23 16:45:43 +0100

    updated as requested by Choco mod

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,22 +2,15 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://www.fiinote.com/pub/appdownload.jsp?os=2'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  url64bit      = $url64
-
   softwareName  = 'FiiNote*'
-
   checksum      = 'A94FD0CDA4746616FA6F38D83AD6A5293015E71AD425AFDA4C14CEE978F49758'
   checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
-
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/S'
 }
--- tools/clean.ps1
+++
@@ -1,5 +0,0 @@
-$f='chocolateyinstall.ps1'
-gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-$f='chocolateyuninstall.ps1'
-gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
